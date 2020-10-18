d51687353ba9ae836a9c45deb986cdb147bbcf05
Dmitry Khartuev <deck92@yandex.ru>
2020-10-18 22:28:55 +0300

    Initial Commit

---
+++ colorPalette/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  colorPalette
+//
+//  Created by Macbook on 18.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var paletteView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //paletteView.layer.cornerRadius = paletteView.frame.width / 2
+
+    }
+
+
+}
+
